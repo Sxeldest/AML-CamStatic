b1d7dac6d84a00a8dd3f145e04ef44fe2ff5048d
Scheldest-Audit <db-audit@github.com>
2026-06-14 18:20:25 +0000

    lalala

--- Settings.cpp
+++ Settings.cpp
@@ -6,10 +6,10 @@ CamSettings g_camSettings;
 
 void LoadCamSettings()
 {
-    g_camSettings.camSensX = cfg->Bind("CamSensX", 1.0f, "Camera")->GetFloat();
-    g_camSettings.camSensY = cfg->Bind("CamSensY", 1.0f, "Camera")->GetFloat();
-    g_camSettings.aimSensX = cfg->Bind("AimSensX", 0.7f, "Camera")->GetFloat();
-    g_camSettings.aimSensY = cfg->Bind("AimSensY", 0.7f, "Camera")->GetFloat();
-    g_camSettings.smoothness = cfg->Bind("Smoothness", 0.5f, "Camera")->GetFloat();
-    g_camSettings.camAccel = cfg->Bind("CamAccel", 1.0f, "Camera")->GetFloat();
+    g_camSettings.camSensX = cfg->Bind("CamSensX", 40.0f, "Camera")->GetFloat();
+    g_camSettings.camSensY = cfg->Bind("CamSensY", 25.0f, "Camera")->GetFloat();
+    g_camSettings.aimSensX = cfg->Bind("AimSensX", 30.0f, "Camera")->GetFloat();
+    g_camSettings.aimSensY = cfg->Bind("AimSensY", 20.0f, "Camera")->GetFloat();
+    g_camSettings.smoothness = cfg->Bind("Smoothness", 12.5f, "Camera")->GetFloat();
+    g_camSettings.camAccel = cfg->Bind("CamAccel", 1.5f, "Camera")->GetFloat();
 }
